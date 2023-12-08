9a8b41797606d79119c890b729f573f77d4e1f6a
Shaun Lee <shaunljw32@gmail.com>
2023-12-08 17:55:36 -0500

    Add an API

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from course import courses
+from student import students
 
 app = FastAPI()
 
-
 @app.get("/courses/{prefix}")
 def get_courses(prefix: str):
-    # return all the courses under the prefix
     results = []
     for course in courses:
         if course.is_prefix(prefix):
             results.append(course)
     
     return results
+
+@app.get("/student_courses/{eid}")
+def get_student_courses(eid: str):
+
+    for student in students:
+        if student.eid == eid:
+            return student.get_registered_courses()
+
+    raise HTTPException(status_code=404, detail="Student with EID " + str(eid) + " not found")
